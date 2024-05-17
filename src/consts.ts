05aa60c878ec85645231e9fe56d914047e336ae3
brian-gee <39035211+brian-gee@users.noreply.github.com>
2024-05-17 09:28:03 -0400

    making progress

---
+++ src/consts.ts
@@ -0,0 +1,15 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Flavia's Sweets";
+export const SITE_DESCRIPTION = "Designing delicious cakes for all occasions.";
+
+export const NAV_ITEMS = [
+	{ text: "Home", href: "#" },
+	{ text: "Why choose us", href: "#features" },
+	{ text: "About Us", href: "#about" },
+	{ text: "Gallery", href: "#gallery" },
+	{ text: "What we make", href: "#products" },
+	{ text: "FAQ", href: "#faq" },
+	{ text: "Contact", href: "#contact" },
+];
