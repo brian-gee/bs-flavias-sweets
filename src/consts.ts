733e9ff21edd45c0d6ff56c04bf4edc31630f20a
brian-gee <39035211+brian-gee@users.noreply.github.com>
2024-05-26 10:36:23 -0400

    need to figure out header

--- src/consts.ts
+++ src/consts.ts
@@ -5,7 +5,6 @@ export const SITE_TITLE = "Flavia's Sweets";
 export const SITE_DESCRIPTION = "Designing delicious cakes for all occasions.";
 
 export const NAV_ITEMS = [
-	{ text: "Home", href: "/" },
 	{ text: "About Us", href: "/#about" },
 	{ text: "Gallery", href: "/#gallery" },
 	{ text: "FAQ", href: "/#faq" },
