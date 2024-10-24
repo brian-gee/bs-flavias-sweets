37b63ed52827be32ae8206cec21c17fbe4c5eb25
brian-gee <39035211+brian-gee@users.noreply.github.com>
2024-10-23 23:09:41 -0400

    changing a lot of spacing and focusing only on mobile for now

--- src/consts.ts
+++ src/consts.ts
@@ -1,12 +1,9 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "Flavia's Sweets";
 export const SITE_DESCRIPTION = "Designing delicious cakes for all occasions.";
 
 export const NAV_ITEMS = [
 	{ text: "About Us", href: "/#about" },
-	{ text: "Gallery", href: "/#gallery" },
+	{ text: "Gallery", href: "https://www.instagram.com/flavias_sweets/" },
 	{ text: "FAQ", href: "/#faq" },
 	{ text: "Contact", href: "/#contact" },
 ];
